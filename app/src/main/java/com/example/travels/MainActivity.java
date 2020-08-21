f3657fcd3a54c0a7e01cd44ca7b986bb699e2c7d
Kimame_04 <mendozakyan@gmail.com>
2020-08-21 20:07:21 +0800

    Added bottom nav and some ui stuff

--- app/src/main/java/com/example/travels/MainActivity.java
+++ app/src/main/java/com/example/travels/MainActivity.java
@@ -1,10 +1,42 @@
 package com.example.travels;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+
+    /*private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener =
+            item -> {
+                if (bottomNavigationView.getSelectedItemId() == item.getItemId())
+                    return true;
+                Fragment selectedFragment = null;
+                switch (item.getItemId()) {
+                    case R.id.profile:
+                        selectedFragment = new ProfileFragment();
+                        break;
+                    case R.id.explore:
+                        selectedFragment = new ExploreFragment();
+                        break;
+                    case R.id.calendar:
+                        selectedFragment = new EventsFragment();
+                        break;
+                    case R.id.settings:
+                        selectedFragment = new SettingsFragment();
+                        break;
+                    case R.id.notifications:
+                        selectedFragment = new NotificationsFragment();
+                        break;
+                }
+                if (androidx.preference.PreferenceManager.getDefaultSharedPreferences(context).getBoolean("settings_animation", true))
+                    getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left).replace(R.id.fragment_container, selectedFragment).commitAllowingStateLoss();
+                else
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commitAllowingStateLoss();
+                return true;
+            };*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
