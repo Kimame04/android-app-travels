4a939fd6dca04588e36864d2904184cff53be55d
Kimame_04 <mendozakyan@gmail.com>
2020-08-21 20:26:31 +0800

    Added shake to send feedback

--- app/src/main/java/com/example/travels/MainActivity.java
+++ app/src/main/java/com/example/travels/MainActivity.java
@@ -1,34 +1,43 @@
 package com.example.travels;
 
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import com.example.travels.fragments.BusArrivalFragment;
+import com.example.travels.fragments.SettingsFragment;
+import com.example.travels.fragments.TrainArrivalFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+import com.squareup.seismic.ShakeDetector;
 
 public class MainActivity extends AppCompatActivity {
+    private Context context;
+    private SensorManager sensorManager;
+    private ShakeDetector shakeDetector;
+    private RelativeLayout relativeLayout;
     private BottomNavigationView bottomNavigationView;
-
-    /*private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener =
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener =
             item -> {
                 if (bottomNavigationView.getSelectedItemId() == item.getItemId())
                     return true;
                 Fragment selectedFragment = null;
                 switch (item.getItemId()) {
-                    case R.id.profile:
-                        selectedFragment = new ProfileFragment();
+                    case R.id.buses:
+                        selectedFragment = new BusArrivalFragment();
                         break;
-                    case R.id.explore:
-                        selectedFragment = new ExploreFragment();
-                        break;
-                    case R.id.calendar:
-                        selectedFragment = new EventsFragment();
+                    case R.id.trains:
+                        selectedFragment = new TrainArrivalFragment();
                         break;
                     case R.id.settings:
                         selectedFragment = new SettingsFragment();
-                        break;
-                    case R.id.notifications:
-                        selectedFragment = new NotificationsFragment();
                         break;
                 }
                 if (androidx.preference.PreferenceManager.getDefaultSharedPreferences(context).getBoolean("settings_animation", true))
@@ -36,11 +45,31 @@ public class MainActivity extends AppCompatActivity {
                 else
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commitAllowingStateLoss();
                 return true;
-            };*/
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = getApplicationContext();
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        shakeDetector = new ShakeDetector(() -> new MaterialAlertDialogBuilder(this)
+                .setTitle("Shake detected").setMessage("Send feedback via email?")
+                .setPositiveButton("Yes", (dialog, which) -> {
+                    String[] TO = {"h1710095@nushigh.edu.sg"};
+                    String[] CC = {};
+                    Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                    emailIntent.setType("text/plain");
+                    emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+                    emailIntent.putExtra(Intent.EXTRA_CC, CC);
+                    emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Put a meaningful subject name here");
+                    emailIntent.putExtra(Intent.EXTRA_TEXT, "Insert your feedback here. Provide feedback constructively!");
+                    try {
+                        startActivity(Intent.createChooser(emailIntent, "Send mail"));
+                    } catch (android.content.ActivityNotFoundException e) {
+                        Snackbar.make(relativeLayout, "You have no email client installed.", BaseTransientBottomBar.LENGTH_SHORT).show();
+                    }
+                }).setNegativeButton("No", null).show());
+        shakeDetector.start(sensorManager);
     }
 }
